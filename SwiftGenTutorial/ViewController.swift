fd31da47013ed536565f9250610228a85c88de33
elhoussaineelgamouz <eelgamouz.ext@deltadore.com>
2024-08-07 12:52:41 +0100

    Initial Commit

---
+++ SwiftGenTutorial/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftGenTutorial
+//
+//  Created by El houssaine El GAMOUZ on 7/8/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
