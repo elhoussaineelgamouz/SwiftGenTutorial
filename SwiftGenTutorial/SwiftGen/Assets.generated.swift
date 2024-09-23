b480d8d9d55acf74343688b0dbc45dde6c3a9f25
elhoussaine elgamouz <elhoussaineelgamouz@gmail.com>
2024-09-23 15:20:50 +0100

    [Feature] Add project files

---
+++ SwiftGenTutorial/SwiftGen/Assets.generated.swift
@@ -0,0 +1,16 @@
+// 🤖 This file was code-generated. Do not edit!!!
+
+import UIKit
+
+// MARK: - Images
+public extension UIImage {
+static let dogIcon: UIImage = UIImage(named: "Dog_icon")!
+static let lionIcon: UIImage = UIImage(named: "Lion_icon")!
+static let tigerIcon: UIImage = UIImage(named: "Tiger_icon")!
+}
+
+// MARK: - Colors
+public extension UIColor {
+static let accentColor: UIColor = UIColor(named: "AccentColor")!
+static let firstColor: UIColor = UIColor(named: "first_color")!
+}
---
+++ SwiftGenTutorial/SwiftGen/Strings.swift
@@ -0,0 +1,8 @@
+//
+//  Strings.swift
+//  SwiftGenTutorial
+//
+//  Created by El houssaine El GAMOUZ on 8/8/2024.
+//
+
+import Foundation
